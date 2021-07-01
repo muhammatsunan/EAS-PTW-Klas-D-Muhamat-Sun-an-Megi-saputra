0c793d9fd96b1b2d60a03f59b76eca6c473b543a
Muhamat Sun'an <muhammatsunan@gmail.com>
2021-07-01 10:43:48 +0700

    first commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('title')
+    Raja Jersey
+@endsection
+
+@section('content')
+    <div class="page-content page-home">
+        <section class="store-carousel">
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-12" data-aos="zoom-in">
+                    <div
+                    id="storeCarousel"
+                    class="carousel slide"
+                    data-ride="carousel"
+                    >
+                    <ol class="carousel-indicators">
+                        
+                    </ol>
+                    <div>
+                        <div>
+                        <img
+                            src="/images/slider1.jpg"
+                            alt="Carousel Image"
+                            class="d-block w-100"
+                        />
+                        </div>
+                        
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
+
+           
+        </div>
+@endsection
